9ccf4a034c3e2d2adc8fdc2cf8e261047960a880
Talan Kerley <Talankerley1@gmail.com>
2025-11-24 11:35:17 +1030

    changed shcema date to python date for proper data validation

--- schemas.py
+++ schemas.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import date
 from typing import Optional
+
 
 class UserCreate(BaseModel):
     name: str
@@ -7,37 +9,42 @@ class UserCreate(BaseModel):
     height_cm: int
     gender: str
 
+
 class WeightCreate(BaseModel):
     user_id: int
-    date: str
+    date: date                     
     weight: float
+
 
 class MeasurementCreate(BaseModel):
     user_id: int
-    date: str
+    date: date                     
     waist_cm: float
     hips_cm: float
     neck_cm: float
     shoulder_cm: float
     chest_cm: float
 
+
 class WorkoutCreate(BaseModel):
     user_id: int
-    date: str
+    date: date                     
     name: str
     exercises_json: str
     duration: Optional[int] = None
+
 
 class PRCreate(BaseModel):
     user_id: int
     exercise_name: str
     weight: float
     reps: int
-    date: str
+    date: date                     
+
 
 class GLP1Create(BaseModel):
     user_id: int
     dose_mg: float
-    date: str
+    date: date                     
     half_life_days: float
     concentration: float
