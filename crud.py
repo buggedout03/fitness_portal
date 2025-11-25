a94767b9bced3c7eebb7dd028e448af5106614c3
Talan Kerley <Talankerley1@gmail.com>
2025-11-26 09:36:12 +1030

    forgot to cleanup crud when mirating dates

--- crud.py
+++ crud.py
@@ -9,12 +9,9 @@ def create_user(db: Session, user: schemas.UserCreate):
     return db_user
 
 def add_weight(db: Session, log: schemas.WeightCreate):
-    data = log.dict()
-    # Ensure date is stored as ISO string
-    data["date"] = log.date.isoformat()
-
-    db_log = models.WeightLog(**data)
+    db_log = models.WeightLog(**log.dict())
     db.add(db_log)
     db.commit()
     db.refresh(db_log)
     return db_log
+
