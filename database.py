0a79fab36fb2ee25df33614c1df103a90b9690d1
Talan Kerley <Talankerley1@gmail.com>
2025-11-25 16:22:56 +1030

    ensure deleting user is safe

--- database.py
+++ database.py
@@ -25,6 +25,12 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
 
     cursor.close()
 
+@event.listens_for(engine, "connect")
+def enable_foreign_keys(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON;")
+    cursor.close()
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
